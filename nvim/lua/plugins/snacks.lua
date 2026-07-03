aaaeaf5dc722a7d466b0f3f4544caf5d481d50a5
Rauls Kjarners <rauls.kjarners@sunfinance.group>
2026-07-03 11:10:30 +0300

    style: enable explorer preview and increase size

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -4,7 +4,23 @@ return {
         opts = {
             picker = {
                 sources = {
-                    explorer = { hidden = true }, -- <leader>e (File Explorer)
+                    explorer = {
+                        hidden = true,
+                        layout = {
+                            preview = true,
+                            layout = {
+                                box = "vertical",
+                                position = "left",
+                                border = "none",
+                                backdrop = false,
+                                height = 0,
+                                width = 60,
+                                { win = "input", height = 1, border = "bottom" },
+                                { win = "list", border = "none" },
+                                { win = "preview", title = "{preview}", height = 0.6, border = "top" },
+                            },
+                        },
+                    }, -- <leader>e (File Explorer)
                     files = { hidden = true }, -- <leader>ff (Find Files)
                     smart = { hidden = true }, -- <leader><space> (Smart Find Files)
                 },
